adf5b11b4b06516b019d0591efd81d67b8e43ed2
Brightness0302 <brightnes0302@gmail.com>
2023-05-16 23:49:49 -0700

    update search form on landing page

--- src/app/(client-components)/(HeroSearchForm)/ButtonSubmit.tsx
+++ src/app/(client-components)/(HeroSearchForm)/ButtonSubmit.tsx
@@ -11,7 +11,7 @@ const ButtonSubmit: FC<Props> = ({ href = "/listing-stay-map" }) => {
     <Link
       href={href}
       type="button"
-      className="h-14 md:h-16 w-full md:w-16 rounded-full bg-primary-6000 hover:bg-primary-700 flex items-center justify-center text-neutral-50 focus:outline-none"
+      className="h-14 md:h-16 w-full md:w-16 rounded-full bg-cornflowerblue hover:bg-primary-700 flex items-center justify-center text-neutral-50 focus:outline-none"
     >
       <span className="mr-3 md:hidden">Search</span>
       <svg
